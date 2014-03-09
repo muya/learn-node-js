d78647cd03b9de2de95f495bb4226cf160a132a5
muya <kingkonig@gmail.com>
2014-03-09 14:17:37 +0300

    added formidable for multi-part file input

---
+++ test_formidable.js
@@ -0,0 +1,26 @@
+var formidable = require('formidable'), http = require('http'), 
+	sys = require('sys');
+
+http.createServer(function (req, res){
+	if(req.url == '/upload' && req.method.toLowerCase() == 'post'){
+		//parse a file upload
+		var form = new formidable.IncomingForm();
+		form.parse(req, function (error, fields, files) {
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.write('received upload:\n\n');
+			res.end(sys.inspect({fields: fields, files: files}));
+		});
+		return;
+	}
+
+	//show a file upload form
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write('<html><head></head><body>' +
+		'<form action="/upload" enctype="multipart/form-data" method="post" '+
+		'<input type="text" name="title"><br>'+
+		'<input type="file" name="upload" multiple="multiple"><br>'+
+		'<input type="submit" value="Upload">'+
+		'</form>' +
+		'</body></html>');
+	res.end();
+}).listen(8888);
